cd6c18b3447459753a07b86c5a827752596b5510
Bruno Bord <bruno.bord@novapost.fr>
2015-09-03 14:07:16 +0200

    simple tests for empty queries on Person autocomplete

--- demo/tests/test_views.py
+++ demo/tests/test_views.py
@@ -16,3 +16,35 @@ class CatalogViewTest(RegistryTestGeneric):
         response = self.client.get(reverse('autocomplete:catalog'))
         data = get_json(response)
         self._test_registry_keys(data)
+
+
+class AutocompleteViewTest(RegistryTestGeneric):
+
+    def test_get_404(self):
+        response = self.client.get(
+            reverse('autocomplete:autocomplete', args=['MEUH']))
+        self.assertEquals(response.status_code, 404)
+
+    def test_autocomplete_person(self):
+        response = self.client.get(
+            reverse('autocomplete:autocomplete', args=['AutocompletePerson']))
+        self.assertEquals(response.status_code, 200)
+
+    def test_autocomplete_person_noquery(self):
+        response = self.client.get(
+            reverse('autocomplete:autocomplete', args=['AutocompletePerson']),
+        )
+        self.assertEquals(response.status_code, 200)
+        data = get_json(response)
+        # No query, the dataset should be empty
+        self.assertFalse(data)
+
+    def test_autocomplete_person_empty_query(self):
+        response = self.client.get(
+            reverse('autocomplete:autocomplete', args=['AutocompletePerson']),
+            data={"q": ""}
+        )
+        self.assertEquals(response.status_code, 200)
+        data = get_json(response)
+        # No query, the dataset should be empty
+        self.assertFalse(data)
