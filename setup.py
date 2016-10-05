50b78e9806c3a6e82450f262b36dc2bb666b81dd
Bruno Bord <bruno.bord@novapost.fr>
2016-10-05 17:13:52 +0200

    v0.6rc4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-agnocomplete',
-    version='0.6rc3',
+    version='0.6rc4',
     packages=['agnocomplete'],
     include_package_data=True,
     description='Frontend-agnostic Django autocomplete utilities',
