c233e09589c614e722c5070937976eb4497ff180
Guillaume Camera <guillaume.camera@novapost.fr>
2018-10-12 17:37:34 +0200

    refs #107 Add a mechanism to validate data in UrlProxy

--- agnocomplete/core.py
+++ agnocomplete/core.py
@@ -17,7 +17,9 @@ from .constants import AGNOCOMPLETE_MIN_PAGESIZE
 from .constants import AGNOCOMPLETE_MAX_PAGESIZE
 from .constants import AGNOCOMPLETE_DEFAULT_QUERYSIZE
 from .constants import AGNOCOMPLETE_MIN_QUERYSIZE
-from .exceptions import AuthenticationRequiredAgnocompleteException, SkipItem
+from .exceptions import AuthenticationRequiredAgnocompleteException
+from .exceptions import SkipItem
+from .exceptions import ItemNotFound
 
 
 logger = logging.getLogger(__name__)
@@ -641,3 +643,27 @@ class AgnocompleteUrlProxy(with_metaclass(ABCMeta, AgnocompleteBase)):
                             )
                         )
         return data
+
+    def validate(self, value):
+        """
+        From a value available on the remote server, the method returns the
+        complete item matching the value.
+        If case the value is not available on the server side or filtered
+        through :meth:`item`, the class:`agnocomplete.exceptions.ItemNotFound`
+        is raised.
+        """
+
+        url = self.get_item_url(value)
+        try:
+            data = self.http_call(url=url)
+        except requests.HTTPError:
+            raise ItemNotFound()
+
+        data = self.get_http_result(data)
+
+        try:
+            self.item(data)
+        except SkipItem:
+            raise ItemNotFound()
+
+        return value
